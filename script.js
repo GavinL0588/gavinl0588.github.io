baeb03022d5645e1c4502d5b7094e757f9ecec10
GavinL0588 <56265226+GavinL0588@users.noreply.github.com>
2019-10-08 10:59:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Gavin Lee"
+let userName = "GavinL0588";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
